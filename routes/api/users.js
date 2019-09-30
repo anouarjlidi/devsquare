d77a68105bd2f1f71797a0eae3d594fb6bceb07a
Tim Moon <tim.moon7@gmail.com>
2019-10-01 00:46:59 +1000

    Add Post

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
